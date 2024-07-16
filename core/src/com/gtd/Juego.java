ac37e69afc23e70737cdbf9a656f55a57cdfa209
Joaquin05Pagano <joaquinpagano05@gmail.com>
2024-07-16 18:24:26 -0300

    puse la portada

--- core/src/com/gtd/Juego.java
+++ core/src/com/gtd/Juego.java
@@ -1,31 +1,33 @@
 package com.gtd;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import utilidades.Render;
 
 public class Juego extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private Texture texturaTierra;
+
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		texturaTierra = new Texture(Gdx.files.internal("PORTADA - copia.jpg"));
 	}
+
+
 
 	@Override
 	public void render() {
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Render.spriteBatch.begin();
+		Render.spriteBatch.draw(texturaTierra, 300f, 150f);
+		Render.spriteBatch.end();
 	}
 	
 	@Override
 	public void dispose() {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
---
+++ core/src/utilidades/Render.java
@@ -0,0 +1,7 @@
+package utilidades;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Render {
+    public static SpriteBatch spriteBatch = new SpriteBatch();
+}
